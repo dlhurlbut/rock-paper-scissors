4d00dde76e7a96b758788c2076a3af5bf6eff482
Denice <dlhurlbut@gmail.com>
2022-03-05 14:52:54 -0600

    Add loop for best of 3 play

--- myScript.js
+++ myScript.js
@@ -1,20 +1,27 @@
-let playerSelection = prompt ("Rock, paper, or scissors?").toLowerCase();
+let computerScore = 0;
+ 
+let playerScore = 0;
 
-let computerPlay = function() {
-    const choices = ["paper", "rock", "scissors"];
-    let computerSelection = choices[Math.floor(Math.random()*3)];
-    alert (("The computer chose ") + computerSelection + ("."));
-    if ((playerSelection == "rock" && computerSelection == "scissors")|| (playerSelection == "scissors" && computerSelection == "paper")|| (playerSelection == "paper" && computerSelection == "rock")) {
-        alert ("You win!");
-    } else if ((playerSelection == "rock" && computerSelection == "paper") || (playerSelection == "scissors" && computerSelection == "rock") || (playerSelection == "paper" && computerSelection == "scissors")) {
-        alert ("You lose!");
-    } else {alert ("It's a tie!")
+function game(){
+
+    for (let i=0;(computerScore<3)&&(playerScore<3); i++){
+        
+        let playerSelection = prompt ("Rock, paper, or scissors?").toLowerCase();
+        
+        function computerPlay() {         
+            const choices = ["paper", "rock", "scissors"];
+            let computerSelection = choices[Math.floor(Math.random() * 3)];
+            alert(("The computer chose ") + computerSelection + ("."));
+            if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "rock")) {
+                alert("You win!");
+            playerScore++;
+            } else if ((playerSelection == "rock" && computerSelection == "paper") || (playerSelection == "scissors" && computerSelection == "rock") || (playerSelection == "paper" && computerSelection == "scissors")) {
+                alert("You lose!");
+                computerScore++;
+            } else {
+                alert("It's a tie!");
+            }
+        }
+        console.log (computerPlay());
     }
 }
-
-console.log(computerPlay());
-    
-       
-           
-
-
